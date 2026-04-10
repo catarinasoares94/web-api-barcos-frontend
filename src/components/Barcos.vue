590cf809b7de76a426c97716b1a2ee7a7dd8c31b
902508016 <902508016@portic.ipp.pt>
2026-04-10 11:16:08 +0100

    US007 implementada
    Implementado o registo de novo barco, não deixa registar se ID já existir, uma vez que é PK. Adicionado o botão para atualizar a lista manualmente

--- src/components/Barcos.vue
+++ src/components/Barcos.vue
@@ -30,7 +30,27 @@
         </tbody>
       </table>
     </div>
-  </div> <!-- DIV DO CONTAINER -->
+
+    <!-- US007 - REGISTAR UM BARCO ( ID, NOME E COR ) -->
+
+    <div class="box">
+      <h2>Registar Barco</h2>
+
+      <form @submit.prevent="registarBarco" class="form">
+        <label>ID</label>
+        <input type="number" v-model="novo.id_barco" placeholder="ID" required />
+
+        <label>Nome</label>
+        <input type="text" v-model="novo.nome" placeholder="Nome" required />
+
+        <label>Cor</label>
+        <input type="text" v-model="novo.cor" placeholder="Cor" required />
+
+        <button type="submit">Registar</button>
+      </form>
+    </div>
+  </div>
+  <!-- DIV DO CONTAINER -->
 </template>
 
 <script>
@@ -40,6 +60,13 @@ export default {
       // US008 - Lista de barcos
       barcos: [],
       mostrarTabela: false,
+
+      // US007 - Registo de Barco
+      novo: {
+        id_barco: '',
+        nome: '',
+        cor: '',
+      },
     }
   },
 
@@ -54,6 +81,41 @@ export default {
       } else {
         this.barcos = []
       }
+    },
+
+    // US007 - Registo de Barco
+    async registarBarco() {
+      const res = await fetch('http://localhost:8080/api/barcos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.novo),
+      })
+
+      const texto = await res.text()
+      console.log(texto)
+
+      if (res.ok) {
+        alert('Barco registado!')
+
+        this.novo = {
+          id_barco: '',
+          nome: '',
+          cor: '',
+        }
+      } else {
+        if (texto.includes('ORA-00001') || texto.toLowerCase().includes('unique')) {
+          alert('ID já existe')
+        } else {
+          alert('Erro ao registar')
+        }
+      }
+    },
+    // Atualizar
+    async atualizarLista() {
+      const res = await fetch('http://localhost:8080/api/barcos')
+      this.barcos = await res.json()
     },
   },
 }
