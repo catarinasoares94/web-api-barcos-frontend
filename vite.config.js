e650153e112dd177fccc8bf47c3ff67d1a62b70e
902508016 <902508016@portic.ipp.pt>
2026-04-12 11:37:58 +0100

    Update vite.config.js
    Configurei o frontend para utilizar um proxy no Vite, permitindo redirecionar os pedidos /api para o backend. Antes os pedidos eram feitos para o localhost, o que impedia o acesso a partir de outros dispositvos

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,19 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.137.1:8080',
+        changeOrigin: true, // adicionei isto para conseguir ver no tablet como "monitor externo"
+      },
     },
   },
 })
